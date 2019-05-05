74b89305509a0dfc46cc2d2a73c0c6c1d4023ff0
hybridarjun <as3999@drexel.edu>
2019-05-05 13:17:02 -0400

    Complete dataset

--- wrangle.py
+++ wrangle.py
@@ -2,12 +2,15 @@
 import json
 import pandas as pd
 import pprint
+
+
 def wrangle():
     files_list = ['14-15','15-16','16-17','17-18']
     team_ids = {}
     matches = {}
     top_ids = {'Chelsea': '15', 'Arsenal': '13', 'Liverpool': '26', 'Manchester United': '32', 'Manchester City':'167', 'Tottenham': '30'}
     season_games = {}
+
     games = {}
     for file in files_list:
         with open('datafile/season{}/season_match_stats.json'.format(file), 'r') as in_file:
@@ -16,8 +19,6 @@ def wrangle():
         with open('datafile/season{}/season_stats.json'.format(file), 'r') as in_file:
 
             match_stats = json.load(in_file)
-
-
 
         for key, val in match_data.items():
 
@@ -52,14 +53,14 @@ def wrangle():
                                                       'total_offside': 0
                                                     }
                             if 'shot_off_target' in val['aggregate_stats']:
-                                season_games[file][key][i]['shots_off_target'] += 1
-                            if 'corners' in val['aggregate_stats']:
-                                season_games[file][key][i]['corners'] += 1
+                                season_games[file][key][i]['shots_off_target'] += int(val['aggregate_stats']['shot_off_target'])
+                            if 'won_corners' in val['aggregate_stats']:
+                                season_games[file][key][i]['corners'] += int(val['aggregate_stats']['won_corners'])
                             if 'total_offside' in val['aggregate_stats']:
-                                season_games[file][key][i]['total_offside'] += 1
+                                season_games[file][key][i]['total_offside'] += int(val['aggregate_stats']['total_offside'])
 
                             if 'goals' in val['aggregate_stats']:
-                                season_games[file][key][i]['goals'] += 1
+                                season_games[file][key][i]['goals'] += int(val['aggregate_stats']['goals'])
 
                             for player_name in val['Player_stats']:
                                 #print(file,player_name,key)
@@ -67,10 +68,24 @@ def wrangle():
                                     season_games[file][key][i]['yellow_card'] += 1
                                 if 'red_card' in val['Player_stats'][player_name]['Match_stats']:
                                     season_games[file][key][i]['red_card'] += 1
+                                if 'fouls' in val['Player_stats'][player_name]['Match_stats']:
+                                    season_games[file][key][i]['fouls'] += int(val['Player_stats'][player_name]['Match_stats']['fouls'])
 
                             i += 1
 
-    df = pd.DataFrame.from_dict(season_games['15-16']['15'])
+#  df = pd.DataFrame.from_dict(season_games['15-16']['15'])
+
+    for top_id in top_ids.values():
+
+        games_df = pd.DataFrame([])
+
+        for file in files_list:
+            df = pd.DataFrame.from_dict(season_games[file][top_id]).T
+
+            games_df = pd.concat([games_df, df], ignore_index=True)
+
+        games_df.to_csv('{}.csv'.format(top_id))
+
 
     pprint.pprint(len(season_games['15-16']['15']))
     print('he''[')
