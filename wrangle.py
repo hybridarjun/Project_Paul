d1b1a224e84912b9d63bfcababa52041ad98ea93
sarthakbababr <sarthakbabbar.07@gmail.com>
2019-05-05 12:32:47 -0400

    fixed season games

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,7 @@
 
 import json
 import pandas as pd
-
+import pprint
 def wrangle():
     files_list = ['14-15','15-16','16-17','17-18']
     team_ids = {}
@@ -30,31 +30,49 @@ def wrangle():
             matches[key] = [val['home_team_id'],val['away_team_id']]
         if file not in season_games:
             season_games[file] = {}
-        i = 1
-        for game_id, stats in match_stats.items():
-            for key,val in stats.items():
-                    if key in top_ids.values():
-                        season_games[file][key][i] = {'possession': val['aggregate_stats']['possession_percentage'],
+
+        for top_id in top_ids.values():
+            i = 1
+            for game_id, stats in match_stats.items():
+                for key,val in stats.items():
+                        if key == top_id:
+                            if key not in season_games[file]:
+                                season_games[file][key] = {}
+                            print(key,i)
+                            season_games[file][key][i] = {'possession': val['aggregate_stats']['possession_percentage'],
                                                       'total_pass': val['aggregate_stats']['total_pass'],
                                                       'total_tackle': val['aggregate_stats']['total_tackle'],
-                                                      'shots_off_target': val['aggregate_stats']['shot_off_target'],
-                                                      'corners': val['aggregate_stats']['won_corners'],
-                                                      'goals': val['aggregate_stats']['goals'],
+                                                      'shots_off_target': 0,
+                                                      'corners': 0,
+                                                      'goals': 0,
                                                       'accurate_pass': val['aggregate_stats']['accurate_pass'],
                                                       'yellow_card':0,
                                                       'red_card':0,
                                                       'fouls':0,
-                                                      'total_offside': val['aggregate_stats']['total_offside']
+                                                      'total_offside': 0
                                                     }
-                    for player_name in val['Player_stats']:
-                        print(file,player_name,key)
-                        if 'yellow_card' in val['Player_stats'][player_name]['Match_stats']:
-                                season_games[file][key][i]['yellow_card'] += 1
-                        if 'red_card' in val['Player_stats'][player_name]['Match_stats']:
-                            season_games[file][key][i]['red_card'] += 1
+                            if 'shot_off_target' in val['aggregate_stats']:
+                                season_games[file][key][i]['shots_off_target'] += 1
+                            if 'corners' in val['aggregate_stats']:
+                                season_games[file][key][i]['corners'] += 1
+                            if 'total_offside' in val['aggregate_stats']:
+                                season_games[file][key][i]['total_offside'] += 1
 
-                    i += 1
+                            if 'goals' in val['aggregate_stats']:
+                                season_games[file][key][i]['goals'] += 1
 
+                            for player_name in val['Player_stats']:
+                                #print(file,player_name,key)
+                                if 'yellow_card' in val['Player_stats'][player_name]['Match_stats']:
+                                    season_games[file][key][i]['yellow_card'] += 1
+                                if 'red_card' in val['Player_stats'][player_name]['Match_stats']:
+                                    season_games[file][key][i]['red_card'] += 1
+
+                            i += 1
+
+    df = pd.DataFrame.from_dict(season_games['15-16']['15'])
+
+    pprint.pprint(len(season_games['15-16']['15']))
     print('he''[')
 if __name__ == '__main__':
     wrangle()
